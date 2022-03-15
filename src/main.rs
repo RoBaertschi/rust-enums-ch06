8309b71e500211925580badd058bf97fd9af649f
RoBaertschi <rtmbaertschi007@gmail.com>
2022-03-15 11:30:53 +0000

    hi

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,17 @@
 fn main() {
-    enum IpAddrKind {
-        V4,
-        V6,
+    enum Message {
+        Quit,
+        Move { x: i32, y: i32 },
+        Write(String),
+        ChangeColor(i32, i32, i32),
     }
 
-    struct IpAddr {
-        kind: IpAddrKind,
-        address: String,
+    impl Message {
+        fn call(&self) {
+            // method body would be defined here
+        }
     }
 
-    let home = IpAddr {
-        kind: IpAddrKind::V4,
-        address: String::from("127.0.0.1"),
-    };
-
-    let loopback = IpAddr {
-        kind: IpAddrKind::V6,
-        address: String::from("::1"),
-    };
+    let m = Message::Write(String::from("hello"));
+    m.call();
 }
